e31a1113e7a9c6b9c19909148bd84eadf7b36a3f
Krasio <krsivanov@gmail.com>
2022-02-10 18:13:16 +0200

    quiz.js modifications

--- quizzes/models.py
+++ quizzes/models.py
@@ -1,5 +1,6 @@
 from statistics import mode
 from django.db import models
+import random
 
 # Create your models here.
 # DIFF_CHOICES = (
@@ -30,7 +31,9 @@ class Quiz(models.Model):
         return f'{self.name} - {self.topic}'
 
     def get_questions(self):
-        return self.question_set.all()[:self.number_of_questions]
+        questions = list(self.question_set.all())
+        random.shuffle(questions)
+        return questions[:self.number_of_questions]
 
     class Meta:
         verbose_name_plural = 'Quizes'
--- quizzes/static/quizzes/quiz.js
+++ quizzes/static/quizzes/quiz.js
@@ -3,26 +3,49 @@ console.log('quiz connected');
 const url = window.location.href
 
 const quizBox = document.getElementById('quiz-box')
-let data
+// let data
 
 $.ajax({
     type: 'GET',
     url : `${url}data`,
     success: function(response){
         // console.log(response);
-        data = response.data
+        const data = response.data
         data.forEach(el => {
             for (const [question, answers] of Object.entries(el)){
                 quizBox.innerHTML += `
                     <hr>
                     <div class="mb-2">
-                        
+                        <b>${question}</b>
                     </div>
                 `
+                answers.forEach(answer=>{
+                    quizBox.innerHTML += `
+                    <div>
+                        <input type="radio" class="ans" id="${question}-${answer}" name="${question}" value="${answer}">
+                        <label for="${question}">${answer}</label>
+                    </div>
+                    `
+                })
             }
         });
     },
     error: function(error){
         console.log(error);
     }
+})
+
+
+const quizForm = document.getElementById('quiz-form')
+const csrf = document.getElementsByName('csrfmiddlewaretoken')
+const elements = [...document.getElementsByClassName('ans')]
+
+const sendData = () => {
+
+}
+
+quizForm.addEventListener('submit', e=> {
+    e.preventDefault()
+
+    sendData()
 })
