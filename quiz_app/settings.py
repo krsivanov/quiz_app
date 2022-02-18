8ac7c19cd53ed8931b2f9c64d70b60d64ce21515
Krasimir Venkov <krsivanov@gmail.com>
2022-02-18 18:18:38 +0200

    quiz app

--- quiz_app/settings.py
+++ quiz_app/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 import dotenv
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -138,3 +139,5 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
     BASE_DIR  / 'quizzes' / 'static',
 ]
+
+django_heroku.settings(locals())
